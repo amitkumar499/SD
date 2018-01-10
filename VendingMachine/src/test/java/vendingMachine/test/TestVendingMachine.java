7bcaf17f0f8e5661889a37f26a49e2e03c2ae48f
amitkumar499 <amitkumar4.akc@gmail.com>
2018-01-10 09:09:36 +0530

    Update TestVendingMachine.java

--- VendingMachine/src/test/java/vendingMachine/test/TestVendingMachine.java
+++ VendingMachine/src/test/java/vendingMachine/test/TestVendingMachine.java
@@ -1,6 +1,8 @@
 package vendingMachine.test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import vendingMachine.enums.Coins;
 import vendingMachine.enums.ItemType;
@@ -13,42 +15,66 @@ public class TestVendingMachine {
 	public static void main(String[] args) {
 		vm = new VendingMachineImpl();
 
-		//insertCoins();
-		buyProduct(ItemType.Coke);
+		selectProduct(ItemType.Coke);
 		refund(ItemType.Coke);
+		selectnBuy(ItemType.Pepsi);
+		reset();
 	}
 
-	/*private static void insertCoins() {
+	/*
+	 * private static void insertCoins() {
+	 * 
+	 * System.out.println("-----Adding Coins-------\n");
+	 * vm.addCoins(Coins.QUARTER); vm.addCoins(Coins.DIME);
+	 * vm.addCoins(Coins.NICKLE); vm.addCoins(Coins.PENNY);
+	 * 
+	 * System.out.println(vm.getTotalCash()+
+	 * "\n Current Balance"+vm.getCurrentBalance()); }
+	 */
 
-		System.out.println("-----Adding Coins-------\n");
-		vm.addCoins(Coins.QUARTER);
-		vm.addCoins(Coins.DIME);
-		vm.addCoins(Coins.NICKLE);
-		vm.addCoins(Coins.PENNY);
+	private static void selectProduct(ItemType item) {
+		System.out.println("\n-----Selecting Product-------\n");
 
-		System.out.println(vm.getTotalCash()+ "\n Current Balance"+vm.getCurrentBalance());
-	}*/
-	
-	private static void buyProduct(ItemType item) {
-		System.out.println("\n-----Buying Product-------\n");
-
-		int price=vm.getItemswithPrice(item);
-		System.out.println(vm.getTotalCash()+ "\n Current Balance"+vm.getCurrentBalance());
+		int price = vm.getItemswithPrice(item);
+		System.out.println("Price of Selected Product : " + price);
 
 	}
-	
+
 	private static void refund(ItemType item) {
-		System.out.println("\n-----Buying Product-------\n");
-		
+		System.out.println("\n-----Refund Money-------\n");
 
 		vm.addCoins(Coins.QUARTER);
-		System.out.println(vm.getTotalCash()+ "\n Current Balance"+vm.getCurrentBalance());
+		System.out.println("Total Coins Left : "+vm.getTotalCash() + "\n Current Balance : " + vm.getCurrentBalance());
 
-		int price=vm.getItemswithPrice(item);
+		vm.getItemswithPrice(item);
 		vm.getRefund();
-		//vm.collectChange();
-		System.out.println(vm.getTotalCash()+ "\n Current Balance"+vm.getCurrentBalance());
+		System.out.println("Total Coins Left : "+vm.getTotalCash() + "\n Current Balance : " + vm.getCurrentBalance());
 
 	}
-	
+
+	private static void selectnBuy(ItemType item) {
+		System.out.println("\n-----Buying Product-------\n");
+
+		vm.addCoins(Coins.QUARTER);
+		vm.addCoins(Coins.QUARTER);
+
+		System.out.println("Total Coins Left : "+vm.getTotalCash() + "\n Current Balance : " + vm.getCurrentBalance());
+
+		vm.getItemswithPrice(item);
+		Map<ItemType, List<Coins>> map = new HashMap<ItemType, List<Coins>>();
+		map = vm.getSelectedProductAndMoney();
+
+		System.out.println("Change Received: " + map.get(item));
+		System.out.println("Total Coins Left : "+vm.getTotalCash() + "\n Current Balance : " + vm.getCurrentBalance());
+
+	}
+
+	private static void reset() {
+		System.out.println("\n-----Reset-------\n");
+
+		System.out.println(vm.getTotalCash());
+		vm.reset();
+		System.out.println(vm.getTotalCash());
+
+	}
 }
